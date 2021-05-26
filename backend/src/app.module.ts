88925f9fc0023dfa65bda998f5bbe92910dd604c
jack <jack@localhost.localdomain>
2021-05-26 19:53:25 +0900

    create base

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BookModule } from './book/book.module';
+import { RentalModule } from './rental/rental.module';
 
 @Module({
-  imports: [],
+  imports: [BookModule, RentalModule],
   controllers: [AppController],
   providers: [AppService],
 })

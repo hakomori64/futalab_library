e33ed8ca5944bf46f26765f883617aadb52c677f
jack <jack@localhost.localdomain>
2021-05-27 21:47:09 +0900

    dto

--- backend/src/book/dto/create-book.dto.ts
+++ backend/src/book/dto/create-book.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateBookDto {
+    @ApiProperty()
     readonly title: string;
+
+    @ApiProperty()
     readonly isbn: string;
+
+    @ApiProperty()
     readonly quantity: number;
 }
